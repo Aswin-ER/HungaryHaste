92c8a5d4ccc5771ee3f1db6c9741d458fa6e4b0a
Aswin Rajan <aswinachu3333@gmail.com>
2024-04-28 12:34:54 +0530

    Implemented the signup page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <ToastContainer theme='dark' autoClose={3000} />
     <Head>
         <script src="https://kit.fontawesome.com/377e6cb833.js" crossOrigin="anonymous" />
       </Head>
---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,130 @@
+import axios from 'axios';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/navigation';
+import React, { FC, useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import * as Yup from 'yup';
+
+const Signin: FC = () => {
+
+    const router = useRouter();
+
+    const [phone, setPhone] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    //Yup
+    const phoneRegExp = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+    const emailRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    const validationSchema = Yup.object().shape({
+        phoneNumber: Yup.string().matches(phoneRegExp, 'Phone number is not valid').required('Phone number is required!'),
+
+        name: Yup.string().min(3, 'Length of the name is too short!').required('Name is required!'),
+
+        email: Yup.string().email('Invlaid email address!').required('Email is required!'),
+        
+        password: Yup.string().matches(emailRegExp, 'Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character').required('Password is required!'),
+        
+    })  
+
+
+    return (
+        <>
+            <div className='flex-1 bg-white'>
+                <h1 className="text-3xl lg:text-4xl font-bold text-center mt-10 mobile:text-2xl "><i className="fa-sharp fa-solid fa-bowl-food px-2"></i>
+                    HungaryHaste
+                </h1>
+                <div className="container mobile:w-10/12 lg:w-8/12  xl:w-4/12 mx-auto px-4 mt-6 lg:mt-16 bg-gray-200 p-8 rounded-lg shadow-lg ">
+                    <h1 className="text-2xl lg:text-4xl font-semibold text-center text-black-700 underline decoration-solid uppercase md:pb-2">
+                        Sign up
+                    </h1>
+
+                    <Formik
+                        initialValues={{ email: '', password: '', phoneNumber: '', name: '' }}
+                        validationSchema={validationSchema}
+                        validateOnChange={true}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setEmail(values?.email);
+                            setName(values?.name);
+                            setPassword(values?.password);
+                            setPhone(values?.phoneNumber);
+
+                            axios.post('http://localhost:5000/api/signup', { email: values?.email, password: values?.password, phone: values?.phoneNumber, name: values?.name}).then((res:any)=> {
+                                if(res.status === 200){                                    
+                                    toast.success(res?.data?.message, {autoClose: 3000});
+                                    setTimeout(() => {
+                                        router.push('/');
+                                    }, 3000);
+                                }else{
+                                    toast.error(res?.data?.message)
+                                    console.log(`Error ${res.data}`);
+                                }
+                            })
+
+                            console.log(values, "values from formik");
+                            setSubmitting(false);
+                        }}
+                    >
+
+                        {({ isSubmitting }) => (
+
+                            <Form className="flex flex-col items-center">
+                                <div className="mb-6 w-4/5">
+                                <label htmlFor='phoneNumber' className="block text-base lg:text-lg font-medium text-gray-800 mb-2">Phone number</label>
+                                <Field type="text" name="phoneNumber" 
+                                id="phoneNumber"
+                                
+                                className="block w-full h-8 lg:px-4 lg:py-2 text-lg lg:text-xl bg-white border border-gray-300 rounded focus:outline-none focus:ring focus:ring-gray-500 focus:border-gray-500 dark:text-black"
+                                />
+                                <div className="text-red-500">
+                                <ErrorMessage name="phoneNumber" component="div" />
+                                </div>
+                                </div>
+
+                                <div className="mb-6 w-4/5">
+                                <label htmlFor='name' className="block text-base lg:text-lg font-medium text-gray-800 mb-2">Name</label>
+                                <Field type="name" name="name" id="name" className="block w-full h-8 lg:px-4 lg:py-2 text-lg lg:text-xl bg-white border border-gray-300 rounded focus:outline-none focus:ring focus:ring-gray-500 focus:border-gray-500 dark:text-black"/>
+                                <div className="text-red-500">
+                                <ErrorMessage name="name" component="div" />
+                                    </div>
+                                </div>
+
+                                <div className="mb-6 w-4/5">
+                                <label htmlFor='email' className="block text-base lg:text-lg font-medium text-gray-800 mb-2">Email</label>
+                                <Field type="email" name="email" id="email" className="block w-full h-8 lg:px-4 lg:py-2 text-lg lg:text-xl bg-white border border-gray-300 rounded focus:outline-none focus:ring focus:ring-gray-500 focus:border-gray-500 dark:text-black" />
+                                <div className="text-red-500">
+                                <ErrorMessage name="email" component="div" />
+                                </div>
+                                </div>
+
+                                <div className="mb-6 w-4/5">
+                                <label htmlFor='password' className="block text-base lg:text-lg font-medium text-gray-800 mb-2">Password</label>
+                                <Field type="password" name="password" id='password' className="block w-full h-8 lg:px-4 lg:py-2 text-lg lg:text-xl bg-white border border-gray-300 rounded focus:outline-none focus:ring focus:ring-gray-500 focus:border-gray-500 dark:text-black"/>
+                                <div className="text-red-500">
+                                <ErrorMessage name="password" component="div" />
+                                </div>
+                                </div>
+
+
+                                
+                                <div className="w-1/2 mt-5">
+                                <button type="submit" disabled={isSubmitting} className="w-full lg:px-1 py-2 mobile:text-sm text-lg lg:text-xl font-medium text-white transition-colors duration-200 transform bg-black rounded-lg hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+                                    Submit
+                                </button>
+                                </div>
+
+                            </Form>
+
+                        )}
+
+                    </Formik>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Signin;
