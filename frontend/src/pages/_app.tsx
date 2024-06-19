60ca701c6e7fdca8a2221dc7872273e6ead53c1b
Aswin Rajan <aswinachu3333@gmail.com>
2024-06-19 23:58:09 +0530

    added layout component for all page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,20 +5,23 @@ import Head from "next/head";
 import { Provider } from "mobx-react";
 import userStoreInstance from "../store/userStore";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Provider userStore={userStoreInstance}>
-        <ToastContainer theme="dark" autoClose={3000} />
-        <Head>
-          <script
-            src="https://kit.fontawesome.com/377e6cb833.js"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <Component {...pageProps} />
+        <Layout>
+          <ToastContainer theme="dark" autoClose={3000} />
+          <Head>
+            <script
+              src="https://kit.fontawesome.com/377e6cb833.js"
+              crossOrigin="anonymous"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </Layout>
       </Provider>
     </>
   );
